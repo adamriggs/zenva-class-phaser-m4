a3d68d29e386a131a899505f566f735c78ebcf35
Adam Riggs <phlux1111@gmail.com>
2018-04-23 23:22:43 -0400

    setting up this lessons game

--- src/states/Game.js
+++ src/states/Game.js
@@ -21,12 +21,21 @@ export default class extends Phaser.State {
     // })
 
     // this.game.add.existing(this.mushroom)
+
+    this.ground = this.add.sprite(0, 500, 'ground')
+
+    var platform = this.add.sprite(0, 300, 'platform')
+
+    this.player = this.add.sprite(100, 200, 'player', 3)
+    this.player.anchor.setTo(0.5)
+    this.player.animations.add('walking', [0, 1, 2, 1], 6, true)
+    this.player.play('walking')
   }
 
   render () {
     // console.log('Game.render()')
     if (__DEV__) {
-      //this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      // this.game.debug.spriteInfo(this.mushroom, 32, 32)
     }
   }
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -13,10 +13,21 @@ export default class extends Phaser.State {
     // load Game assets
     //
     this.load.image('mushroom', 'assets/images/mushroom2.png')
+
+    this.load.image('ground', 'assets/images/ground.png')
+    this.load.image('platform', 'assets/images/platform.png')
+    this.load.image('goal', 'assets/images/gorilla3.png')
+    this.load.image('arrowButton', 'assets/images/arrowButton.png')
+    this.load.image('actionButton', 'assets/images/actionButton.png')
+    this.load.image('barrel', 'assets/images/barrel.png')
+
+    this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1)
+    this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1)
   }
 
   create () {
     console.log('Splash.create()')
+    this.game.stage.backgroundColor = '#000'
     this.state.start('Game')
   }
 }
