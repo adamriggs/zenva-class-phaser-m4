74063971ccb620afc41b6c08af30696ad842000d
Adam Riggs <phlux1111@gmail.com>
2018-04-26 08:35:17 -0400

    m4 - loading json files

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,6 +1,7 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
+import level from '../level'
 
 export default class extends Phaser.State {
   init () {
